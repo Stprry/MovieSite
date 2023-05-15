f6ec91e0c22992e41367c34efb409b69ba591f8a
Sam Perry <samperry.dev@gmail.com>
2023-05-15 20:36:14 +0100

    Nav at bot

--- Movies/src/routes/+page.svelte
+++ Movies/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto w-3/4 flex justify-center">
+   <div class="block fixed inset-x-12 bottom-4 z-10 bg-red-500 rounded-lg py-4 px-4 shadow">
+        <div class="">
+            h
+        </div>
+   </div>
+</div>
