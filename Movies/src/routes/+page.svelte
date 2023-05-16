8a60d60eb552e065880aa9730c658cc2e46d619c
Sam Perry <samperry.dev@gmail.com>
2023-05-16 20:58:54 +0100

    Nav bar spacing

--- Movies/src/routes/+page.svelte
+++ Movies/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <div class="container mx-auto w-3/4 flex justify-center">
-   <div class="block fixed inset-x-12 bottom-4 z-10 bg-red-500 rounded-lg py-4 px-4 shadow">
-        <div class="">
-            h
-        </div>
-   </div>
+	<div
+		class="inline-flex fixed inset-x-12 bottom-4 z-10 bg-red-500 rounded-lg py-4 px-4 shadow justify-around"
+	>
+		<div class="">Home</div>
+		<div>Rate</div>
+		<div>Sign In</div>
+	</div>
 </div>
